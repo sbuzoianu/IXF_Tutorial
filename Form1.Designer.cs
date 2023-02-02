b69b4a327af01ea238834b16c5efc6b264de7d92
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2023-02-02 11:42:57 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace IXF_Tutorial {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.BackColor = System.Drawing.Color.Yellow;
+            this.SubmitBtn.Location = new System.Drawing.Point(209, 112);
+            this.SubmitBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(102, 62);
+            this.SubmitBtn.TabIndex = 0;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = false;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(209, 59);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.SubmitBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.SubmitBtn);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.TextBox numberTextBox;
+    }
+}
+
